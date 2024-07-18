9e4d346a168612d256620d7fdaf53eef25c4edf6
KANAMARLAPUDI VENKATA SUBBARAO <subbaraokvsr890@gmail.com>
2024-07-18 14:59:38 +0530

    added file

---
+++ Operators/Equality operator.py
@@ -0,0 +1,23 @@
+'''
+3. Equality operator:
+* It is used to compare the value of given 2 arguments.
+* There are : == , !=
+
+Example: 
+10=='sunny' #False 
+10==10.0 #True
+10=='10' #False
+10.10 == 10.1 # True
+
+'''
+
+a = 40
+b = 20
+
+print(a==b)
+print(a!=b)
+
+print(10=='sunny')
+print(10==10.0)
+print(10=='10')
+print(10.10 == 10.1)
