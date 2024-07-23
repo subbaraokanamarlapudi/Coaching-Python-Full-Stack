e86119f97465976127452474b6fb21a315d0c6ba
KANAMARLAPUDI VENKATA SUBBARAO <subbaraokvsr890@gmail.com>
2024-07-23 10:55:39 +0530

    assignment operator file added

---
+++ Operators/Assignment operator.py
@@ -0,0 +1,30 @@
+'''
+We can use assignment operator to assign a value to the variable.
+Ex:
+		x = 10
+
+-->We can cobmine assignment operator with some other operator to form compound assignment operator.
+Ex:
+		x += 10 ===>x = x+10
+
+-->Compound assignment operators are:
+		+=,-=,*=,/=,..............
+
+-->No increment and decrement operators in python.
+Ex:
+		x++(Invalid)
+		x--(Invalid)
+		+(+x) ==>(valid)
+		-(-x)==>(valid)
+
+'''
+
+x = 4  #Assignemnt operator
+x += 1
+x -= 2
+x *= 3
+x /= 4
+x //= 5
+x %= 6
+x **= 7
+print(x)
