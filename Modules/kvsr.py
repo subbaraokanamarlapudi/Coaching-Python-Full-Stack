4dea80df5a45a9d61d955a6051c320fb9a498399
KANAMARLAPUDI VENKATA SUBBARAO <subbaraokvsr890@gmail.com>
2024-08-20 11:18:25 +0530

    special variable

--- Modules/kvsr.py
+++ Modules/kvsr.py
@@ -4,3 +4,12 @@ def add(a,b):
     return a+b
 def product(a,b):
     return a*b
+
+def f1():
+    if __name__ == '__main__':
+        print('The code executed directly as a program')
+        print('The value of __name__:',__name__)
+    else:
+        print('The code executed indirectly as a module from some other module')
+        print('The value of __name__:',__name__)
+f1()
