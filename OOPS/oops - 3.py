f4fa4ad2e13f3f108e690dab50b03ee3a2e281b8
KANAMARLAPUDI VENKATA SUBBARAO <subbaraokvsr890@gmail.com>
2024-09-21 11:45:02 +0530

    Polymorphism

---
+++ OOPS/oops - 3.py
@@ -0,0 +1,163 @@
+'''
+POLYMORPHISM :
+--------------
+* Poly means Many
+* Morphs means Forms
+* Polymorphism means 'Many Forms'
+
+Ex:
+	Yourself is best example for polymorphism
+	Vikram-->Aparichithudu
+
+    
+1).Duck Typing philosophy of python
+2).Overloading:
+		-->Operator overloading
+		-->Method overloading
+		-->Constructor overloading
+3).Overriding:
+		-->Method overriding
+		-->Constructor overriding
+'''
+
+'''
+1).Duck Typing philosophy of python
+--------------------------------------
+* In python we cannot specify the type explicitly.
+* Based on provides value at runtime the type will be considerd automatically.
+* Hence python is considered as dynamically typed programming language.
+
+        def f1(obj):
+            obj.talk()
+
+What is the type of obj?
+-------------------------
+* We cannot decide at the beginning. At runtime we can pass any type. Then how we can decide the type.
+* At runtime if 'it walks like a duck and talks like a duck,it must be duck'.
+* Python follows this principle. This is called as Duck Typing philosophy of python
+
+'''
+
+# Examople
+
+# class Duck:
+#     def talk(self):
+#         print('Quack...... Quack.......')
+
+# class Dog:
+#     def bark(self):
+#         print('Bow........... Bow........')
+
+# class Cat:
+#     def talk(self):
+#         print('Meow........ Meow........')
+
+# class Goat:
+#     def talk(self):
+#         print('Myaahh........ Myaahh........')
+
+# def f1(obj):
+#     obj.talk()
+
+# l=[Duck(),Dog(),Cat(),Goat()]
+# for obj in l:
+#     f1(obj)
+
+# -->The problem in this approach is if obj does not contain talk() method then we will get AttributeError.
+# 			AttributeError: 'Dog' object has no attribute 'talk'
+# -->We can solve this problem by using hasattr() function.
+# 					hasattr(obj,'attributename')
+# -->attributename can be method name or variable name.
+
+# Example
+
+# class Dog:
+# 	def bark(self):
+# 		print('Bow...Bow...')
+# class Human:
+# 	def talk(self):
+# 		print('Hello...Hi.....')
+
+# def f1(obj):
+# 	if hasattr(obj,'talk'):
+# 		obj.talk()
+# 	elif hasattr(obj,'bark'):
+# 		obj.bark()
+
+# l = [Dog(),Human()]
+# for obj in l:
+# 	f1(obj)
+
+# Overloading:
+# ===========
+# 	We can use same operator or methods for different purposes.
+
+# 1).Operator Overloading:
+# 	We can use same operator for multiple purposes, which is nothing but operator overloading.
+
+# Ex:
+	10 + 20 #30
+	'10' + '20' #'1020'
+
+# Ex:To use + operator for our class objects
+# ---------------------------------------------------------------
+class Book:
+	def __init__(self,pages):
+		self.pages = pages
+b1 = Book(100)
+b2 = Book(200)
+print(b1 + b2)
+
+# TypeError: unsupported operand type(s) for +: 'Book' and 'Book'
+
+# -->We can overload + operator to work with Book objects also.
+# -->For every operator magic methods are available. To overload any operator we have to override that method in our class.
+# -->Internally + operator is implemented by using __add__() method. We have to override this method in our class.
+
+# Ex:To overload + operator for our Book class objects
+# -------------------------------------------------------------------------------
+class Book:
+	def __init__(self,pages):
+		self.pages = pages
+	def __add__(self,other):
+		return self.pages + other.pages
+b1 = Book(100)
+b2 = Book(200)
+print('The total number of pages:',b1 + b2)
+
+# How to add multiple objects
+# ------------------------------------------
+class Book:
+	def __init__(self,pages):
+		self.pages = pages
+	def __add__(self,other):
+		print('add method calling')
+		total = self.pages + other.pages
+		return Book(total)
+	def __str__(self):
+		return str(self.pages)
+b1 = Book(100)
+b2 = Book(200)
+b3 = Book(300)
+bx = b1 + b2 + b3
+#print(bx.pages)
+print(bx)
+
+# Ex:To overload multiplication operator to work on Employee objects
+# ------------------------------------------------------------------------------------------------------
+class Employee:
+	def __init__(self,name,sal):
+		self.name = name
+		self.sal = sal
+	def __mul__(self,other):
+		return self.sal * other.days
+class TimeSheet:
+	def __init__(self,name,days):
+		self.name = name
+		self.days = days
+	def __mul__(self,other):
+		return self.days * other.sal
+e = Employee('Sunny',800)
+t = TimeSheet('Sunny',25)
+print('This month salary:',e * t)
+print('This month salary:',t * e)
