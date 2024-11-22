e67880c575b01ff157f8d90d5e1424b5333382fe
KANAMARLAPUDI VENKATA SUBBARAO <subbaraokvsr890@gmail.com>
2024-11-22 15:29:25 +0530

    det() and solve() functions of matrix and I/O operations with numpy

--- n3.py
+++ n3.py
@@ -183,3 +183,166 @@ np.dot(np.linalg.inv(a),np.linalg.inv(a))
 np.linalg.matrix_power(np.linalg.inv(a),2)
 
 # Note:We can find matix_power only for a square matrix, otherwise we will get error.
+
+# 3.det():To find determinant of a matrix
+# ---------------------------------------------
+# help(np.linalg.det)
+# print(help(np.linalg.det))
+
+a = np.array([[1,2],[3,4]])
+a
+
+# For 3x3 matrix:
+# ------------------
+
+# a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+a = np.arange(9).reshape(3,3)
+a
+np.linalg.det(a)
+
+# Note: We can find determinant only for square matrix.
+
+# 4.solve():To solve linear algebra equations
+# -----------------------------------------------
+# help(np.linalg.solve)
+# print(help(np.linalg.solve))
+
+'''
+case study:
+-------------
+Boys + Girls = 2200
+Boys = 3$   ,   Girls = 8$
+Total Fees  = 10100$
+
+x + y = 2200   ------> x = 2200 - y
+3x + 8y = 10100
+
+3(2200-y) + 8y = 10100
+6600 - 3y + 8y = 10100
+5y = 10100 - 6600
+5y = 3500
+y = 3500/5
+y = 700
+
+x = 2200 - 700
+x = 1500
+
+
+'''
+
+a = np.array([[1,1],[3,8]])
+a
+b = np.array([2200,10100])
+b
+np.linalg.solve(a,b)
+
+# Note:We can solve linear algebra equations only for square matrix, otherwise we will get an error.
+
+# Ex: 
+a = np.array([-4,7,-2],[1,-2,1],[2,-3,1])
+a
+b = np.array([2,3,-4])
+b
+np.linalg.solve(a,b)
+
+# ----------------------------------------- I/O Operations with numpy ----------------------------------------------------------------
+
+# We can save/write ndarray objects to a binary file for further purpose.Later point of time,whenever these objects are required, we can read from that binary file.
+
+# save() ----> To save/write ndarray object to a file.
+# load() ----> To read ndarray object from a file.
+
+print(help(np.save()))
+print(help(np.load()))
+
+# EX - 1: Saving ndarray object to a file and read back
+# ------------------------------------------------------------
+
+import numpy as np
+a = np.array([10,20,30],[40,50,60])
+
+# save/serialize ndarray object to a file
+np.save('out.npy',a)
+
+# load ndarray objectt from a file
+out_array = np.load('out.npy')
+print(out_array)
+
+# Note : 
+# 1. The data will be stored in binary format.
+# 2. File extension should be .npy, otherwise save() function itself will add that file extension.
+# 3. By using save() function we can write only one object to the file. If we want to write multiple objects to a file then we should go for save() function. 
+
+# Saving multiple ndarray objects to the binary file:
+# --------------------------------------------------------------
+
+import numpy as np
+a = np.array([10,20,30],[40,50,60])
+b = np.array([70,80],[90,100])
+
+np.savez('out.npz',a,b)
+
+npzfileobj = np.load('out.npz')  # returns NpzFile object
+
+print(type(npzfileobj))
+print(npzfileobj.files)
+print(npzfileobj['arr_0'])
+print(npzfileobj['arr_1'])
+
+# Note: 
+# 1. np.save() ---> save an array to a binary file in .npy format.
+# 2. np.savez() ---> save several arrays into a single file in .npz format but in uncompressed form.
+# 3. np.savez.compressed() ---> save several arrays into a single file in .npz format but in compressed form.
+# 4. np.load() ---> To load/read ndarray from .npy or .npz file.
+
+# Compressed form:
+# --------------------
+np.savez_compressed('out_compressed.npz',a,b)
+
+# Analysis:
+# ---------
+# path of the out_compressed.npz file
+
+# Q : we can save object in compressed form,then what is the need of uncompressed form?
+
+# compressed form ----> Memory will be saved , but performance will be degraded.
+# uncompressed form ----> Memory will be wasted, but performance will be increased.
+
+# Save ndarray objects to the file in normal text format:
+# --------------------------------------------------------------
+# savetxt() and loadtxt()
+
+print(np.savetxt())
+print(np.loadtxt())
+
+# Ex:
+import numpy as np
+a = np.array([10,20,30],[40,50,60])
+np.savetxt('out.txt',a)
+
+out_array = np.loadtxt('out.txt')
+print(out_array)
+
+print('output array in the format')
+out_array2 = np.loadtxt('out.txt',delimiter=',')
+print(out_array2)
+
+# Ex:
+
+import numpy as np
+a = np.array([['sunny',1000],['rainy',2000],['cloudy',3000]])
+np.savetxt('out.txt',a,fmt='%s %d',delimiter=',')
+
+a2 = np.loadtxt('out.txt',delimiter=',')
+print(a2)
+
+# Writing ndarray objects to the csv file:
+# --------------------------------------------
+# csv - comma separated values
+
+import numpy as np
+a1 = np.array([10,20,30],[40,50,60])
+np.savetxt('out.csv',a1,fmt='%d',delimiter=',')
+
+a2 = np.loadtxt('out.csv',delimiter=',')
+print(a2)
